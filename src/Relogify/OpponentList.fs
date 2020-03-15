6c142e78fdd9e2d25079377207e66726c6efde38
Kyrylo Yakymenko <yakimych@gmail.com>
2020-03-15 11:07:31 +0100

    Add wiring for fetchPlayers command

--- src/Relogify/OpponentList.fs
+++ src/Relogify/OpponentList.fs
@@ -1,24 +1,70 @@
 module Relogify.OpponentList
 
+open Fabulous
 open Fabulous.XamarinForms
 open Xamarin.Forms
 
 type Model =
-    { OpponentNames: string list }
+     { CurrentPlayerName: string
+       OpponentNames: string list }
 
-let initModel = { OpponentNames = [ "Kalle"; "Leif" ] }
+let initModel =
+    { CurrentPlayerName = "TestPlayer"
+      OpponentNames = [ "PlaceHolder" ] }
 
-let view model =
+type Msg =
+    | FetchPlayersClicked
+    | PlayersFetched of string list
+
+type CmdMsg = FetchPlayersCmdMsg
+
+let fetchPlayersCmd () =
+    async {
+        do! Async.Sleep 200
+        return PlayersFetched ["Kalle"; "Leif"]
+    }
+    |> Cmd.ofAsyncMsg
+
+let mapCommands =
+    function
+    | FetchPlayersCmdMsg -> fetchPlayersCmd()
+
+let update model msg: Model * CmdMsg list =
+    match msg with
+    | FetchPlayersClicked -> model, [FetchPlayersCmdMsg]
+    | PlayersFetched players -> { model with OpponentNames = players }, []
+
+let view model dispatch =
     View.ContentPage(
         title = "Select Opponent",
         content = View.CollectionView(
             items = [
-                View.StackLayout
-                    (padding = Thickness 20.0, verticalOptions = LayoutOptions.Center,
+                View.StackLayout(
+                     padding = Thickness 20.0,
+                     verticalOptions = LayoutOptions.Center,
                      children =
-                         [ View.Label
-                             (text = "Placeholder", horizontalOptions = LayoutOptions.Center, width = 200.0,
-                              horizontalTextAlignment = TextAlignment.Center) ])
-                    ]
+                         [
+                             yield View.Label(
+                                text = model.CurrentPlayerName,
+                                horizontalOptions = LayoutOptions.Center,
+                                width = 200.0,
+                                horizontalTextAlignment = TextAlignment.Center)
+
+                             yield! model.OpponentNames
+                                    |> List.map (fun opponentName ->
+                                    View.Label(
+                                           text = opponentName,
+                                           horizontalOptions = LayoutOptions.Center,
+                                           width = 200.0,
+                                           horizontalTextAlignment = TextAlignment.Center)
+                                    )
+
+                             yield View.Button(
+                                text = "Fetch Players",
+                                horizontalOptions = LayoutOptions.Center,
+                                width = 200.0,
+                                command = (fun () -> dispatch FetchPlayersClicked))
+                         ])
+                ]
             )
         )
