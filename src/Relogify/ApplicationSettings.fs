ef127e3d9db0e08003fbc967d03328b2b78deb85
Kyrylo Yakymenko <yakimych@gmail.com>
2020-03-21 02:09:38 +0100

    Save and read settings (part1: community name)

---
+++ src/Relogify/ApplicationSettings.fs
@@ -0,0 +1,28 @@
+module Relogify.ApplicationSettings
+
+open System
+open Xamarin.Forms
+
+type ApplicationSettings = {
+    CommunityName: string option
+    PlayerName: string option
+}
+
+let CommunityNameStorageKey = "community_name"
+let PlayerNameStorageKey = "player_name"
+
+let getStringValueOrNone key =
+    let found, property = Application.Current.Properties.TryGetValue(key)
+    if found then Some(property.ToString()) else None
+
+let getApplicaitonSettings () =
+    { CommunityName = getStringValueOrNone CommunityNameStorageKey
+      PlayerName = getStringValueOrNone PlayerNameStorageKey }
+
+let saveApplicationSettings (communityName: string) (playerName: string) =
+    if not (String.IsNullOrEmpty(communityName)) then
+        Application.Current.Properties.[CommunityNameStorageKey] <- communityName
+    if not (String.IsNullOrEmpty(playerName)) then
+        Application.Current.Properties.[PlayerNameStorageKey] <- playerName
+
+    Application.Current.SavePropertiesAsync()
