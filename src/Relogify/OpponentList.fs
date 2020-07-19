dd8365bc82ca8957bfe48af3c74d06004d995934
Kyrylo Yakymenko <yakimych@gmail.com>
2020-07-20 01:23:56 +0200

    Display communities from settings in opponent list

--- src/Relogify/OpponentList.fs
+++ src/Relogify/OpponentList.fs
@@ -6,14 +6,18 @@ open Relogify.ApplicationSettings
 open Relogify.Graphql
 open Xamarin.Forms
 
+//type CommunityWithPlayers = { communityName: string; playerNames: string list }
+
+type CommunitiesWithPlayers = Map<string, string []>
+
 type Model =
     | Fetching of communities: Community list
-    | FetchSuccess of playerNames: string list
+    | FetchSuccess of communitiesWithPlayers: CommunitiesWithPlayers * currentCommunityName: string
     | FetchError of communities: Community list * errorMessage: string
 
 type Msg =
     | FetchPlayers of Community list
-    | PlayersFetched of string list
+    | PlayersFetched of CommunitiesWithPlayers
     | FetchPlayersError of communities: Community list * errorMessage: string
     | PlayerSelected of int option
 
@@ -22,12 +26,25 @@ type CmdMsg =
     | DeselectPlayerCmdMsg
 
 type OutMsg =
-    | PlayerSelectedOutMsg of player: string
+    | PlayerSelectedOutMsg of player: string * communityName: string
 
 let initModel (communities : Community list): Model * CmdMsg list =
     match communities with
     | [] -> FetchError ([], "Application settings are empty"), []
     | _ -> Fetching communities, [FetchPlayersCmdMsg communities]
+
+// TODO: Remove
+type TempType = {
+    playerName: string
+    communityName: string
+}
+
+// TODO: Rewrite without intermediate type
+let toCommunitiesWithPlayers (fetchedPlayers: MyProvider.Operations.GetPlayersForCommunities.Types.Query_root): CommunitiesWithPlayers =
+    let asd = fetchedPlayers.Players |> Array.map (fun p -> { playerName = p.Name; communityName = p.Community.Name })
+    let qwe = asd |> Array.groupBy (fun p -> p.communityName) //|> Array.map (fun x -> ())
+    let zxc = qwe |> Array.map (fun (communityName, tempType) -> (communityName, tempType |> Array.map (fun z -> z.playerName)))
+    zxc |> Map.ofArray
 
 let fetchPlayersCmd (communities: Community list) =
     async {
@@ -36,7 +53,7 @@ let fetchPlayersCmd (communities: Community list) =
         return
             match result.Data with
 //            | Some data -> data.Players |> List.ofArray |> List.map (fun p -> p.Name) |> List.except [currentPlayerName] |> PlayersFetched
-            | Some data -> data.Players |> List.ofArray |> List.map (fun p -> p.Name) |> PlayersFetched
+            | Some data -> data |> toCommunitiesWithPlayers |> PlayersFetched
             | None -> FetchPlayersError (communities, sprintf "Error fetching players for communities '%A'. Please check your internet connection and try again. If the problem persists, check your settings and restart the app" communityNames)
     }
     |> Cmd.ofAsyncMsg
@@ -55,16 +72,17 @@ let mapCommands: (CmdMsg -> Cmd<Msg>) =
 let update (model: Model) (msg: Msg) (currentPlayerName: string): Model * CmdMsg list * OutMsg option =
     match msg with
     | FetchPlayers communities -> Fetching communities, [FetchPlayersCmdMsg communities], None
-    | PlayersFetched players -> FetchSuccess players, [], None
+    | PlayersFetched players -> FetchSuccess (players, "test"), [], None
     | FetchPlayersError (communities, errorMessage) -> FetchError (communities, errorMessage), [], None
     | PlayerSelected maybeIndex ->
         match maybeIndex, model with
-        | Some index, FetchSuccess players ->
-            let selectedPlayer = players.[index]
-            model, [DeselectPlayerCmdMsg], Some <| PlayerSelectedOutMsg selectedPlayer
+        | Some index, FetchSuccess (communitiesWithPlayers, selectedCommunityName) ->
+            let selectedCommunity = communitiesWithPlayers.[selectedCommunityName]
+            let selectedPlayer = selectedCommunity.[index]
+            model, [DeselectPlayerCmdMsg], Some <| PlayerSelectedOutMsg (selectedPlayer, selectedCommunityName)
         | _ -> model, [], None
 
-let view (model: Model) (dispatch: Msg -> unit): ViewElement =
+let view (allCommunities: Community list) (model: Model) (dispatch: Msg -> unit): ViewElement =
     View.ContentPage(
         title = "Players",
         icon = ImagePath "tab_feed.png",
@@ -88,21 +106,19 @@ let view (model: Model) (dispatch: Msg -> unit): ViewElement =
                         )
                     ]
                 )
-            | FetchSuccess players ->
+            | FetchSuccess (communitiesWithPlayers, communityName) ->
+                let players = communitiesWithPlayers.[communityName]
                 View.StackLayout(
                     orientation = StackOrientation.Vertical,
                     children = [
                         View.StackLayout(
                             orientation = StackOrientation.Horizontal,
-                            children = [
-                                View.Button(text = "test")
-                                View.Button(text = "test2")
-                            ]
+                            children = (allCommunities |> List.map (fun community -> View.Button(text = community.CommunityName)))
                         )
                         View.ListView(
                             ref = listViewRef,
                             isPullToRefreshEnabled = false,
-                            items = (players |> List.map (fun playerName -> View.TextCell(text = playerName))),
+                            items = (players |> Array.map (fun playerName -> View.TextCell(text = playerName)) |> List.ofArray),
                             itemSelected = (dispatch << PlayerSelected)
                         )
                     ]
