8e1142c9867d987ca134e959bd68e7543150fc96
Kyrylo Yakymenko <yakimych@gmail.com>
2020-10-02 14:52:45 +0200

    Implement challenge-response happy-case flows

--- src/Relogify.Android/MyBroadcastReceiver.fs
+++ src/Relogify.Android/MyBroadcastReceiver.fs
@@ -2,22 +2,24 @@ namespace Relogify.Android
 
 open Android.Content
 open Android.Support.V4.App
+open Relogify
 
 [<BroadcastReceiver(Enabled = true, Exported = false)>]
 type MyBroadcastReceiver() =
     inherit BroadcastReceiver()
 
     override this.OnReceive(context: Context, intent: Intent) =
-        // TODO: extract player name and community name in order to respond
+        // TODO: Special template, so that we don't have to parse the string
         let challengeFrom = intent.GetStringExtra("CHALLENGE_FROM")
         let communityName = intent.GetStringExtra("COMMUNITY_NAME")
 
-        Relogify.ApplicationSettings.removeChallengeFromLocalStorage { PlayerName = challengeFrom; CommunityName = communityName } |> Async.RunSynchronously |> ignore
+        ApplicationSettings.removeChallengeFromLocalStorage { PlayerName = challengeFrom; CommunityName = communityName }
+        |> Async.RunSynchronously
+        |> ignore
 
         match intent.Action with
         | "ACTION_ACCEPT" ->
-            // TODO: Send back response
-            ()
+            ChallengeManager.respondToChallenge challengeFrom communityName |> Async.RunSynchronously
         | "ACTION_DECLINE" ->
             // TODO: Send back response
             ()
