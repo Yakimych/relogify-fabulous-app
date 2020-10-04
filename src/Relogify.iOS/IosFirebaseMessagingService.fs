0c693e676aa726155e246e2c10302bf7901837ba
Kyrylo Yakymenko <yakimych@gmail.com>
2020-10-04 22:30:11 +0200

    Implement empty MessagingService for iOS in order to avoid it crashing

---
+++ src/Relogify.iOS/IosFirebaseMessagingService.fs
@@ -0,0 +1,11 @@
+﻿namespace Relogify.iOS
+
+open Xamarin.Forms
+
+//[<Service>]
+type IosFirebaseMessagingService() =
+    interface Relogify.IMessagingService with
+        member this.SendRegistrationToServer (): Async<unit> = async { return () }
+
+[<assembly: Dependency(typeof<IosFirebaseMessagingService>)>]
+do ()
